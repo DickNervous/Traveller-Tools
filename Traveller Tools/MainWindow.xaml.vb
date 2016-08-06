849c64085c24a9877c877a9535a8e2d3bb22501a
Dick Nervous <Dick Nervous@NORMANDY>
2016-08-06 00:38:03 -0400

    Add project files.

---
+++ Traveller Tools/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs) Handles MyBase.Loaded
+
+        Dim Traveller_System_DataDataSet As Traveller_Tools.Traveller_System_DataDataSet = CType(Me.FindResource("Traveller_System_DataDataSet"), Traveller_Tools.Traveller_System_DataDataSet)
+
+        'Load data into the table System_Master. You can modify this code as needed.
+        Dim Traveller_System_DataDataSetSystem_MasterTableAdapter As Traveller_Tools.Traveller_System_DataDataSetTableAdapters.System_MasterTableAdapter = New Traveller_Tools.Traveller_System_DataDataSetTableAdapters.System_MasterTableAdapter()
+        Traveller_System_DataDataSetSystem_MasterTableAdapter.Fill(Traveller_System_DataDataSet.System_Master)
+
+        Dim System_MasterViewSource As System.Windows.Data.CollectionViewSource = CType(Me.FindResource("System_MasterViewSource"), System.Windows.Data.CollectionViewSource)
+        System_MasterViewSource.View.MoveCurrentToFirst()
+
+    End Sub
+End Class
---
+++ Traveller Tools/Traveller_Tools/Traveller_System_DataDataSet.vb
@@ -0,0 +1,12 @@
+﻿Namespace Traveller_Tools
+    Friend Class Traveller_System_DataDataSetTableAdapters
+        Friend Class System_MasterTableAdapter
+            Public Sub New()
+            End Sub
+
+            Friend Sub Fill(system_Master As Traveller_System_DataDataSet.System_MasterDataTable)
+                Throw New NotImplementedException()
+            End Sub
+        End Class
+    End Class
+End Namespace
